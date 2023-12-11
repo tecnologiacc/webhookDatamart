6927d6d0131f2f6ad2dd71b52652e85478e6f6d7
BTbackT <fblanco@crececapital.pe>
2023-12-11 10:22:51 -0500

    feat: Test added

--- test.py
+++ test.py
@@ -2,10 +2,11 @@ import unittest
 import requests
 
 class DatamartTests(unittest.TestCase):
-    base_url = 'http://127.0.0.1:5000'
+    base_url = 'http://twh.factura-negociable.net.pe:7074'
 
-    def test_datamart_active(self):
-        URL = f'{self.base_url}/download/RUC/20601099251'
+    # ------------------- TESTS DE RUC -------------------
+    def test_ruc_datamart_active(self):
+        URL = f'{self.base_url}/download/RUC/20506151547'
 
         # Enviar solicitud
         r = requests.get(URL)
@@ -13,7 +14,12 @@ class DatamartTests(unittest.TestCase):
         # Verificar respuesta
         self.assertEqual(r.status_code, 200)
 
-    def test_datamart_inactive(self):
+        if r.status_code == 200:
+            # Verificar mensaje de respuesta (JSON)
+            self.assertEqual(r.json()['code'], '200')
+            self.assertEqual(r.json()['message'], 'Documento descargado')
+
+    def test_ruc_datamart_inactive(self):
         URL = f'{self.base_url}/download/RUC/0'
 
         # Enviar solicitud
@@ -21,3 +27,54 @@ class DatamartTests(unittest.TestCase):
 
         # Verificar respuesta
         self.assertEqual(r.status_code, 400)
+
+        if r.status_code == 400:
+            # Verificar mensaje de respuesta (JSON)
+            self.assertEqual(r.json()['code'], '301')
+            self.assertEqual(r.json()['message'], 'Subscripcion inactiva')
+
+    # ------------------- TESTS DE RTT -------------------
+    def test_rtt_datamart_active(self):
+        URL = f'{self.base_url}/download/RTT/20506151547'
+
+        # Enviar solicitud
+        r = requests.get(URL)
+
+        # Verificar respuesta
+        self.assertEqual(r.status_code, 200)
+
+        if r.status_code == 200:
+            # Verificar mensaje de respuesta (JSON)
+            self.assertEqual(r.json()['code'], '200')
+            self.assertEqual(r.json()['message'], 'Documento descargado')
+
+    def test_rtt_datamart_inactive(self):
+        URL = f'{self.base_url}/download/RTT/0'
+
+        # Enviar solicitud
+        r = requests.get(URL)
+
+        # Verificar respuesta
+        self.assertEqual(r.status_code, 400)
+
+        if r.status_code == 400:
+            # Verificar mensaje de respuesta (JSON)
+            self.assertEqual(r.json()['code'], '301')
+            self.assertEqual(r.json()['message'], 'Subscripcion inactiva')
+
+    def test_type_invalid(self):
+        URL = f'{self.base_url}/download/XXX/20506151547'
+
+        # Enviar solicitud
+        r = requests.get(URL)
+
+        # Verificar respuesta
+        self.assertEqual(r.status_code, 400)
+
+        if r.status_code == 400:
+            # Verificar mensaje de respuesta (JSON)
+            self.assertEqual(r.json()['code'], '302')
+            self.assertEqual(r.json()['message'], 'Tipo de documento no permitido')
+
+if __name__ == '__main__':
+    unittest.main()
