65afdf638625cdcb172ce6a1753c6400ff049ee3
BT <fblanco@crececapital.pe>
2023-12-11 18:41:24 -0500

    Update datamart.py

--- datamart.py
+++ datamart.py
@@ -1,6 +1,7 @@
-from os import environ
+from datetime import datetime
 from dotenv import load_dotenv
 from flask import Flask, request, Response
+from os import environ
 
 import json
 import os
@@ -16,48 +17,76 @@ username = environ.get('USER')
 password = environ.get('PASSWORD')
 driver = environ.get('DRIVER')
 
-# Conectarse con la DB
-conn = pyodbc.connect(f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}')
-
 app = Flask(__name__)
 
-@app.route('/download/<type>/<id>', methods = ['GET'])
-def download_document(type, id):
-    json_response = {
-        'code': '',
-        'message': '',
-        'ruta': ''
-    }
-    if request.method == 'GET':
-        # Verificar si esta suscrito en la API Datamart
-        URL = f'https://api.datamart.pe/subscriptions/v1/check-subscription/20601610664/{id}/RTTSync?check-cred-status=true'
-        header = {
-            'x-api-key': 'GQeqgT2mcc9W6ee7J1yPV3xApGBI6Cno2xwLtEri',
-        }
-        r = requests.get(URL, headers=header)
+def verify_datamart_subscription(id):
+    """ Verificar si el usuario esta suscrito en la API Datamart
+    
+    Parameters
+    ----------
+    id : str
+        Clave SOL del cliente
+    Returns
+    -------
+    response : requests.models.Response
+        Respuesta de la API Datamart
 
-        # Verificar si se obtuvo una respuesta
+    """
+    URL = 'https://api.datamart.pe/subscriptions/v1/check-subscription/20601610664/20601610664/RTTSync?check-cred-status=true'
+    header = {
+        'x-api-key': 'GQeqgT2mcc9W6ee7J1yPV3xApGBI6Cno2xwLtEri',
+    }
+
+    return requests.get(URL, headers = header)
+
+@app.route('/download/RUC/<id>', methods = ['GET'])
+def download_ruc_document(id):
+    """ Descargar el documento PDF del RUC
+
+    Parameters
+    ----------
+    id : str
+        Número de RUC del cliente
+    Returns
+    -------
+    response : flask.wrappers.Response
+        Respuesta del endpoint
+    """
+    try:
+        json_response = {
+            'code': '',
+            'message': '',
+            'ruta': ''
+        }
+
+        # Verificar si esta suscrito en la API Datamart
+        r = verify_datamart_subscription(id)
         if r.status_code != 200:
             json_response['code'] = '300'
             json_response['message'] = 'Error al consultar la API'
             return Response(
                 json.dumps(json_response),
-                status=400,
-                mimetype='application/json'
+                status = 400,
+                mimetype = 'application/json'
             )
 
         # Confirmar `subscripcion activa` campo `EstadoCredencial`
-        datamart_response = r.json()
-        estado_credencial = datamart_response['EstadoCredencial']
+        datamart_json = r.json()
+        estado_credencial = datamart_json['EstadoCredencial']
         if estado_credencial != 'CredencialValida':
             json_response['code'] = '301'
             json_response['message'] = 'Subscripcion inactiva'
             return Response(
                 json.dumps(json_response),
-                status=400,
-                mimetype='application/json'
+                status = 400,
+                mimetype = 'application/json'
             )
-        
+
+        # Crear la conexión a la DB
+        conn = pyodbc.connect(
+            f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+        )
+
         # Crear un `cursor` para iterar sobre la DB
         cursor = conn.cursor()
 
@@ -66,34 +95,12 @@ def download_document(type, id):
             SELECT TOP 1 PERCENT
                 *
             FROM {database}.dbo.Resp_Webhook
+            WHERE Respuesta LIKE '%"RUC": "{id}%' 
+            ORDER BY FechaRegistro DESC
         """
 
-        # Filtrar por `tipo` de documento
-        if type == 'RUC':
-            query += f"""
-                WHERE Respuesta LIKE '%"RUC": "{id}%' 
-                ORDER BY FechaRegistro DESC
-            """
-        elif type == 'RTT':
-            query += f"""
-                WHERE Respuesta LIKE '%"SubscriberId": "{id}%'
-                AND DATEDIFF(day, FechaRegistro, GETDATE()) <= 45
-                ORDER BY FechaRegistro DESC
-            """
-        else:
-            json_response['code'] = '302'
-            json_response['message'] = 'Tipo de documento no permitido'
-            return Response(
-                json.dumps(json_response),
-                status=400,
-                mimetype='application/json'
-            )
-
-        # Ejecutar la query
-        cursor.execute(query)
-
-        # Obtener los resultados
-        result = cursor.fetchall()
+        # Ejecutar la query y obtener los resultados
+        result = cursor.execute(query).fetchall()
         cursor.close()
 
         # Verificar si hay resultados
@@ -102,8 +109,8 @@ def download_document(type, id):
             json_response['message'] = 'Documento no encontrado'
             return Response(
                 json.dumps(json_response),
-                status=400,
-                mimetype='application/json'
+                status = 400,
+                mimetype = 'application/json'
             )
 
         # Transformar el string a JSON
@@ -112,19 +119,16 @@ def download_document(type, id):
         # Usar EnlacePDF
         URL = respuesta['EnlacePdf']
         # Descargar el documento PDF
-        r = requests.get(URL, allow_redirects=True)
+        r = requests.get(URL, allow_redirects = True)
 
-        # Generar el `path` para el documento con el formato: fecha_id_RTT.pdf
+        # Generar el `path` para el documento con el formato: fecha_id_RUC.pdf
         formato_fecha = result[0][0].strftime('%Y%m%d%H%M%S%f')[:-4]
         ruta = os.path.join(os.path.expanduser('~'), 'Downloads')
-        # ruta = 'C:/websites/webhookpdfdemo'
-        file = ''
-        if type == 'RUC':
-            file = f'{formato_fecha}_{id}_FRUC.pdf'
-        elif type == 'RTT':
-            file = f'{formato_fecha}_{id}_RTT.pdf'
+        file = f'{formato_fecha}_{id}_FRUC.pdf'
 
+        # Verificar estado de la descarga
         if r.status_code == 200:
+            # Guardar el documento en la ruta especificada
             with open(os.path.join(ruta, file), 'wb') as f:
                 f.write(r.content)
         else:
@@ -132,27 +136,179 @@ def download_document(type, id):
             json_response['message'] = 'Documento no disponible para descarga'
             return Response(
                 json.dumps(json_response),
-                status=400,
-                mimetype='application/json'
+                status = 400,
+                mimetype = 'application/json'
             )
 
-        # Verificar si el documento esta descargado
-        # en la carpeta `downloads`
+        # Verificar si el documento se ha guardado
         if os.path.isfile(os.path.join(ruta, file)):
             json_response['code'] = '200'
             json_response['message'] = 'Documento descargado'
         else:
             json_response['code'] = '305'
             json_response['message'] = 'Documento no descargado'
+            return Response(
+                json.dumps(json_response),
+                status = 400,
+                mimetype = 'application/json'
+            )
+
         json_response['ruta'] = os.path.join(ruta, file)
 
         return Response(
             json.dumps(json_response),
-            status=200,
-            mimetype='application/json'
+            status = 200,
+            mimetype = 'application/json'
+        )
+    except Exception as e:
+        json_response['error'] = str(e)
+        return Response(
+            json.dumps(json_response),
+            status = 500,
+            mimetype = 'application/json'
         )
 
-    return Response(json_response, status=400, mimetype='application/json')
+@app.route('/download/RTT/<id>', methods = ['GET'])
+def download_rtt_document(id):
+    """ Descargar el documento PDF del RTT
+
+    Parameters
+    ----------
+    id : str
+        Número de RUC del cliente
+    Returns
+    -------
+    response : flask.wrappers.Response
+        Respuesta del endpoint
+    """
+    try:
+        json_response = {
+            'code': '',
+            'message': '',
+            'ruta': ''
+        }
+
+        # Verificar si esta suscrito en la API Datamart
+        r = verify_datamart_subscription(id)
+        if r.status_code != 200:
+            json_response['code'] = '300'
+            json_response['message'] = 'Error al consultar la API'
+            return Response(
+                json.dumps(json_response),
+                status = 400,
+                mimetype = 'application/json'
+            )
+
+        # Confirmar `subscripcion activa` campo `EstadoCredencial`
+        datamart_json = r.json()
+        estado_credencial = datamart_json['EstadoCredencial']
+        if estado_credencial != 'CredencialValida':
+            json_response['code'] = '301'
+            json_response['message'] = 'Subscripcion inactiva'
+            return Response(
+                json.dumps(json_response),
+                status = 400,
+                mimetype = 'application/json'
+            )
+
+        # Crear la conexión a la DB
+        conn = pyodbc.connect(
+            f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+        )
+
+        # Crear un `cursor` para iterar sobre la DB
+        cursor = conn.cursor()
+
+        # Buscar el último documento registrado en la DB
+        query = f"""
+            SELECT TOP 1 PERCENT
+                *
+            FROM {database}.dbo.Resp_Webhook
+            WHERE Respuesta LIKE '%"SubscriberId": "{id}%' 
+            ORDER BY FechaRegistro DESC
+        """
+
+        # Ejecutar la query y obtener los resultados
+        result = cursor.execute(query).fetchall()
+        cursor.close()
+
+        # Verificar si hay resultados
+        if len(result) == 0:
+            json_response['code'] = '303'
+            json_response['message'] = 'Documento no encontrado'
+            return Response(
+                json.dumps(json_response),
+                status = 400,
+                mimetype = 'application/json'
+            )
+
+        # Verificar vigencia de la subscripcion < 45 dias
+        fecha_registro = result[0][0]
+        fecha_actual = datetime.now()
+        dias = (fecha_actual - fecha_registro).days
+        if dias > 45:
+            json_response['code'] = '306'
+            json_response['message'] = 'Documento fuera de vigencia'
+            return Response(
+                json.dumps(json_response),
+                status = 400,
+                mimetype = 'application/json'
+            )
+
+        # Transformar el string a JSON
+        respuesta = json.loads(result[0][1])
+
+        # Usar EnlacePDF
+        URL = respuesta['EnlacePdf']
+        # Descargar el documento PDF
+        r = requests.get(URL, allow_redirects = True)
+
+        # Generar el `path` para el documento con el formato: fecha_id_RUC.pdf
+        formato_fecha = result[0][0].strftime('%Y%m%d%H%M%S%f')[:-4]
+        ruta = os.path.join(os.path.expanduser('~'), 'Downloads')
+        file = f'{formato_fecha}_{id}_FRTT.pdf'
+
+        # Verificar estado de la descarga
+        if r.status_code == 200:
+            # Guardar el documento en la ruta especificada
+            with open(os.path.join(ruta, file), 'wb') as f:
+                f.write(r.content)
+        else:
+            json_response['code'] = '304'
+            json_response['message'] = 'Documento no disponible para descarga'
+            return Response(
+                json.dumps(json_response),
+                status = 400,
+                mimetype = 'application/json'
+            )
+
+        # Verificar si el documento se ha guardado
+        if os.path.isfile(os.path.join(ruta, file)):
+            json_response['code'] = '200'
+            json_response['message'] = 'Documento descargado'
+        else:
+            json_response['code'] = '305'
+            json_response['message'] = 'Documento no descargado'
+            return Response(
+                json.dumps(json_response),
+                status = 400,
+                mimetype = 'application/json'
+            )
+
+        json_response['ruta'] = os.path.join(ruta, file)
+
+        return Response(
+            json.dumps(json_response),
+            status = 200,
+            mimetype = 'application/json'
+        )
+    except Exception as e:
+        json_response['error'] = str(e)
+        return Response(
+            json.dumps(json_response),
+            status = 500,
+            mimetype = 'application/json'
+        )
 
 if __name__ == '__main__':
     app.run()
